ab57ece662ba71b7b90e04cc9651468da148703c
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-07-05 00:15:03 -0700

    update

--- setup/htmlize_this_file.el
+++ setup/htmlize_this_file.el
@@ -3,6 +3,10 @@
 
 (setq enable-local-variables nil)
 
+(defun makefile-p (file)
+  (string-equal (file-name-sans-extension (file-name-nondirectory file))
+                "Makefile"))
+
 (let* ((tmpbuf (find-file (concat default-directory "tmpfile")))
        (output-file (concat default-directory "output-file"))
        (argv
@@ -14,28 +18,26 @@
     (let ((common-root output-dir))
       (while (not (string-match-p (regexp-quote common-root) file))
         (setq common-root (file-name-directory common-root)))
-      (let* ((outfile
+      (let* ((outfile-nonhtml
               (expand-file-name
                (concat
                 output-dir "/"
                 (replace-regexp-in-string
-                 (regexp-quote common-root) "" file)
-                ".html")))
+                 (regexp-quote common-root) "" file))))
+             (outfile (concat outfile-nonhtml ".html"))
              (outfile-dir (file-name-directory outfile)))
         (unless (file-exists-p outfile-dir)
           (make-directory outfile-dir))
         (with-temp-buffer
-          (insert (format "%s => %s\n" (file-relative-name file)
-                          (file-relative-name outfile)))
+          (insert (format
+                   (concat "%s => %s"
+                           (if (makefile-p file) ".html" "{,.html}") "\n")
+                   (file-relative-name file)
+                   (file-relative-name outfile-nonhtml)))
           (append-to-file (point-min) (point-max) output-file))
-        (let ((file-buf (find-file file)))
-          (kill-buffer
-           (with-current-buffer
-               (with-current-buffer file-buf
-                 (font-lock-fontify-buffer)
-                 (htmlize-buffer (current-buffer)))
-             (write-file outfile)))
-          (kill-buffer file-buf)))))
+        (htmlize-file (expand-file-name file) outfile)
+        (unless (makefile-p file)
+          (copy-file (expand-file-name file) outfile-nonhtml t t t t)))))
 
   (defadvice org-publish-get-project-from-filename (around ew activate)
     (setq ad-return-value (car org-publish-project-alist)))
